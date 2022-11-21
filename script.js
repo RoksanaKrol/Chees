7f8b793c7b63506c77191f4598ce54ae918bfa6a
RoksanaKrol <roksanakrol613@gmail.com>
2022-11-21 16:55:10 +0100

    board numbers

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 function createBoard() {
     var board = "";
-    var a = "abcdefgh";
+    var a = " ABCDEFGH";
     
     board +="<table>";
-        for(i=0; i<8; i++) {
+        for(i=0; i<9; i++) {
         board+="<td>"+a[i]+"</td>";
     }
     for (var i=8; i>0; i--) {
         board +="<tr>";
-        for (var j=0; j<8; j++) {
-            board +="<td><div id='"+a[j]+i+"'class='field ";
-            if (i%2==0) {
-                if (j%2==0) {
-                    board +="white";
-                } else {
-                    board +="black";
-                }
+        for (var j=0; j<10; j++) {
+            if (j==0 || j==9) {
+                board+="<td class='num'>"+i+"</td>";
             } else {
-                if (j%2==0) {
-                    board +="black";
+                board +="<td><div id='"+a[j]+i+"'class='field ";
+                if (i%2==0) {
+                    if (j%2==0) {
+                        board +="white";
+                    } else {
+                        board +="black";
+                    }
                 } else {
-                    board +="white";
-                }
-            }    
-            board +="'>"+a[j]+i+"</div></td>";
+                    if (j%2==0) {
+                        board +="black";
+                    } else {
+                        board +="white";
+                    }
+                }    
+                board +="'>"+a[j]+i+"</div></td>";
+            }
         }
       board +="</tr>";
     }
-        for(i=0; i<8; i++) {
+        for(i=0; i<9; i++) {
         board+="<td>"+a[i]+"</td>";
     }
     board +="</table>";
